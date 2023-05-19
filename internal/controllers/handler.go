e1afca7b116779d192ff23a5693416aa4bad5208
Roman <safronovraff@gmail.com>
2023-05-19 18:22:41 +0300

    fix handler Increase end add map

--- internal/controllers/handler.go
+++ internal/controllers/handler.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"io"
 	"net/http"
+	"strings"
 )
+
+var urlMap = make(map[string]string)
 
 func Shorten(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -16,10 +19,14 @@ func Shorten(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+
 	short := string(resp)
+	shortURL := shortenURL(string(resp))
+	urlMap[short] = short
+
 	w.Header().Set("content-type", "text/plain;charset=utf-8 ")
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(short))
+	w.Write([]byte(shortURL))
 }
 
 func Increase(w http.ResponseWriter, r *http.Request) {
@@ -27,12 +34,18 @@ func Increase(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Only GET requests are allowed!", http.StatusBadRequest)
 		return
 	}
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		http.Error(w, "The query parameter is missing", http.StatusBadRequest)
-		return
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	url, ok := urlMap[id]
+	if !ok {
+		http.Error(w, "invalid URL ID", http.StatusBadRequest)
 	}
 
-	w.Header().Set("Location", id)
+	w.Header().Set("Location", url)
 	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+// Функция для генерации сокращенного URL
+func shortenURL(url string) string {
+	//TODO тут  будет алгоритм генерации сокращенного URL
+	return "short_" + url
 }
