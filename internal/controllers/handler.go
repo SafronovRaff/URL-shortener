13cfc64c8280376944701c35ca7110cb69976f64
Roman <safronovraff@gmail.com>
2023-05-22 13:33:13 +0300

    fix shortenURL

--- internal/controllers/handler.go
+++ internal/controllers/handler.go
@@ -2,11 +2,21 @@ package controllers
 
 import (
 	"github.com/go-chi/chi/v5"
-
 	"io"
+	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
 var urlMap = make(map[string]string)
 
 func Shorten(w http.ResponseWriter, r *http.Request) {
@@ -55,6 +65,27 @@ func Increase(w http.ResponseWriter, r *http.Request) {
 
 // Функция для генерации сокращенного URL
 func shortenURL(url string) string {
-	//TODO тут  будет алгоритм генерации сокращенного URL
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(15) - 5
+	log.Printf("рандом длинны строки %d", n)
+	res := randStringBytes(n)
+	log.Printf("рандом строка %s", res)
+	url = res
 	return url
+}
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return string(b)
 }
