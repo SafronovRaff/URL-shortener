bd9f22f2d89c10104ca81b114a7a284c3b6d3c12
Roman <safronovraff@gmail.com>
2023-05-23 12:50:43 +0300

    fix bugs url.ParseRequestURI

--- internal/controllers/handler.go
+++ internal/controllers/handler.go
@@ -67,7 +67,6 @@ func Increase(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "параметр id пуст", http.StatusBadRequest)
 		return
 	}
-	//parsedURL, err := url.ParseRequestURI(id)
 
 	parsedURL, err := url.Parse(id)
 	if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
