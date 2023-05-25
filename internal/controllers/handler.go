0b236bcb6497b6d8a8edb8902088cc7400519a7a
Roman <safronovraff@gmail.com>
2023-05-25 15:17:18 +0300

    add logic

--- internal/controllers/handler.go
+++ internal/controllers/handler.go
@@ -8,32 +8,10 @@ import (
 	"net/url"
 )
 
-type Result struct {
-	Link   string
-	Code   string
-	Status int
-}
-
 var urlmap map[string]string
 
 func Shorten(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "<h1>hellow</h1>")
-	//templ, _ := template.ParseFiles("templates/index.html")
-	//result := Result{}
-	//if r.Method == "POST" {
-	//	if !isValidUrl(r.FormValue("s")) {
-	//		log.Printf("Что-то не так")
-	//		result.Status = 400
-	//		result.Link = ""
-	//	} else {
-	//		result.Link = r.FormValue("s")
-	//		result.Code = maintenance.GenerateRandomString(10)
-	//		maintenance.NewMap().Add(result.Link, result.Code)
-	//		log.Printf("Добавлен URL в urlMap. Ключ: %s, Значение: %s", result.Link, result.Code)
-	//		result.Status = http.StatusCreated
-	//	}
-	//}
-	//templ.Execute(w, result)
+
 	// считываем данные из тела запроса
 	b, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -69,16 +47,6 @@ func Shorten(w http.ResponseWriter, r *http.Request) {
 }
 
 func Increase(w http.ResponseWriter, r *http.Request) {
-
-	//	id := mux.Vars(r)
-	//
-	//originalURL, err := maintenance.NewMap().Get(id["key"])
-	////log.Printf("Извлечен URL из urlMap. Ключ: %s, Значение: %s, Найден: %v", decodedURL, originalURL, err)
-	//if err != nil {
-	//	http.Error(w, "недопустимый идентификатор URL-адреса", http.StatusNotFound)
-	//	return
-	//}
-	//fmt.Fprintf(w, "<script>location='%s';</script>", originalURL)
 
 	id := r.URL.Path
 	log.Printf("URL.Path: %s", id)
@@ -121,11 +89,12 @@ func Increase(w http.ResponseWriter, r *http.Request) {
 		return
 	}*/
 	originalURL, ok := urlmap[id]
-	log.Printf("originalURL: %s", originalURL)
+	log.Printf("найден originalURL: %s", originalURL)
 	if !ok {
-		http.Error(w, "url не найден", http.StatusBadRequest)
+		http.Error(w, "originalURL не найден", http.StatusBadRequest)
 	}
 	// Возвращаем оригинальный URL
+
 	w.Header().Set("Location", originalURL)
 	w.WriteHeader(http.StatusTemporaryRedirect)
 	//http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
